0387cfd74fc570504edd4ccdf76f75a2c63d187f
ramiro orozco <orozco.ramiro@gmail.com>
2022-05-24 16:21:28 -0300

    cambios menú

--- login.php
+++ login.php
@@ -13,9 +13,14 @@ include 'checkBrowser.php';
 <head>
 <?php include '../css/header.php'; ?>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="css/style.css">
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" href="css/style2.css">
 <title>Login</title>
 
 </head>
@@ -29,9 +34,13 @@ include 'checkBrowser.php';
 	
 
 	<header>
-		<h1 align="center">Gestión de Locales</h1>
+		<h1 align="center">XL GESTION</h1>
 
 	</header>
+
+	<aside id="logo">
+		<img src="Controlador/logo.jpg">
+	</aside>
 
 	<?php 
 		// if(get_the_browser() == 'Google Chrome'){
@@ -40,53 +49,36 @@ include 'checkBrowser.php';
 	
 	<section id="contenido">
 		<article>
-			<form action="Controlador/validar.php" method="post">
-				<div class="form-group row">
-
-				<div class="col-7">
-
-					<input class="form-control" type="text" id="usuarioRegistrado" name="user" placeholder="Usuario" required autofocus>
-
+			<form action="Controlador/validar.php" method="post">	
+				<div class="container login-form">
+					<h2 class="login-title">- Please Login -</h2>
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<form>
+								<div class="input-group login-userinput">
+									<span class="input-group-addon"><span class="fa fa-user mr-3" id="userIcon"></span></span>
+									<input class="form-control" type="text" id="usuarioRegistrado" name="user" placeholder="Usuario" required autofocus>
+								</div>
+								<div class="input-group">
+									<span class="input-group-addon"><span class="fa fa-lock mr-3" id="userIcon"></span></span>
+									<input class="form-control" type="password" value="hunter2" id="example-password-input" placeholder="Contraseña" name="pass" required>
+									<span id="showPassword" class="input-group-btn">
+							<button class="btn btn-default reveal" type="button"><i class="glyphicon glyphicon-eye-open"></i></button>
+						</span>  
+								</div>
+								<button class="btn btn-primary btn-block login-button" type="submit"><i class="fa fa-sign-in"></i> Ingresar</button>
+								<div class="checkbox login-options mt-2">
+									<label for="">Conexión con el local</label>
+									<select id="inputState" name="conecta" >
+										<option selected value="si">Si</option>
+										<option value="no">No</option>
+									</select>	
+								</div>		
+							</form>			
+						</div>
+					</div>
 				</div>
-
-				</div>
-
-				<div class="form-group row">
-
-				<div class="col-7">
-
-					<input class="form-control" type="password" value="hunter2" id="example-password-input" placeholder="Contraseña" name="pass" required>
-					
-				</div>
-				
-				</br>
-				
-				
-
-				
-				
-				
-				</div>
-								
-				
-				</br>
-				Conexión con el local
-				
-				
-				<select id="inputState" name="conecta" >
-				<option selected value="si">Si</option>
-				<option value="no">No</option>
-				</select>
-				
-				</br>
-				</br>
-				
-				
-				
-				<button type="submit" class="btn btn-primary">Ingresar</button>
-
-
-
+										
 				<div class="alert alert-danger mt-2" role="alert" id="label" style="display:none" >
 				<a > No existe el usuario! </a>
 				</div>
@@ -95,10 +87,6 @@ include 'checkBrowser.php';
 			</form>
 		</article>
 	</section>
-
-	<aside>
-		<img src="Controlador/logo.jpg">
-	</aside>
 
 	<?php
 		}else{
@@ -124,6 +112,34 @@ include 'checkBrowser.php';
 
 <script src="Controlador/validar_usuario.js"></script>
 
+<script>
+
+window.onload = function(){$("#showPassword").hide();}
+
+$("#txtPassword").on('change',function() {  
+		if($("#txtPassword").val())
+		{
+			$("#showPassword").show();
+		}
+		else
+		{
+			$("#showPassword").hide();
+		}
+});
+
+$(".reveal").on('click',function() {
+    var $pwd = $("#txtPassword");
+    if ($pwd.attr('type') === 'password') 
+		{
+        $pwd.attr('type', 'text');
+    } 
+		else 
+		{
+        $pwd.attr('type', 'password');
+    }
+});
+
+</script>
 
 </html>
 
