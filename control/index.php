93e37f286edf23c3f601010084bb6600b1bfd2c7
Gaston Marcilio <gaston.marcilio@gmail.com>
2022-11-24 18:11:41 -0300

    fix 2411

--- control/index.php
+++ control/index.php
@@ -17,7 +17,6 @@ if (!isset($_SESSION['username'])) {
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<title>Control de Remitos</title>
-		<?php include '../../css/header.php'; ?>
 	</head>
 
 	<body>
