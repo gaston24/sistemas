7698aa6d977752de4a37cd85e565a8a9b9506c6b
Gaston Marcilio <gaston.marcilio@gmail.com>
2023-03-15 09:46:08 -0300

    check cant controlada

--- control/controlador/procesarRemito.php
+++ control/controlador/procesarRemito.php
@@ -18,29 +18,37 @@ try {
 	$articulosControlados = $_post['data'];
 	$acum = 0;
 
+
 	foreach($articulosControlados as $art){
-
-
-
-		$codArticu = $art[0];
-		$cantRem = $art[1];
 		$cantControl = $art[2];
 		$acum += $cantControl;
-		$status = ($cantRem <> $cantControl) ? 'PENDIENTE' : 'ACEPTADO';
-
-		if( in_array(substr(strtoupper($codArticu), 0, 1) , ['X', 'O']) ){
-			$remito->insertarAuditoria($fechaRem, $codClient, $rem, $sucOrig, $sucDestin, $codArticu, $cantRem, $cantControl, $codVend, $status);
-		}
-
 	}
+
 
 	if ( $acum > 0 ){
 
+		foreach($articulosControlados as $art){
+
+			$codArticu = $art[0];
+			$cantRem = $art[1];
+			$cantControl = $art[2];
+
+			$status = ($cantRem <> $cantControl) ? 'PENDIENTE' : 'ACEPTADO';
+
+			if( in_array(substr(strtoupper($codArticu), 0, 1) , ['X', 'O']) ){
+				$remito->insertarAuditoria($fechaRem, $codClient, $rem, $sucOrig, $sucDestin, $codArticu, $cantRem, $cantControl, $codVend, $status);
+			}
+
+		}
+
 		$remito->ajusteRemitoStatus($rem);
 
-	}else {
+		echo true;
+
+	}else{
+
+		echo false;
 		
-		$remito->borrarRemitoControlado($rem);
 	}
 
 } catch (\Throwable $th) {
