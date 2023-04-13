4b8555001da7bd30b06e71350c52917893ff9a22
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-04-13 14:55:07 -0300

    13/4

--- Controlador/validar.php
+++ Controlador/validar.php
@@ -47,7 +47,7 @@ if( count($loginRes) == 0 ){
 	// total de deuda
 	$_SESSION['totalDeuda'] = $loginRes['TOTAL_DEUDA'];
 
-	$_SESSION['pantallas'] = true;
+	$_SESSION['pantallas'] = false;
 
 	if($_SESSION['pantallas'] == true ){
 		header("Location: ../indexPantalla.php");
