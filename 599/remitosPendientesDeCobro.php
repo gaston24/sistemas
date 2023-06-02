b7f119e4cf05e72b9298a72a5cac8afb3c0957e9
sistemas.143 <sistemas@xl.com.ar>
2023-06-02 15:29:26 -0300

    cambio Dani liberar caché

--- 599/remitosPendientesDeCobro.php
+++ 599/remitosPendientesDeCobro.php
@@ -11,6 +11,9 @@ $detalleDeRemito = traerDetalle($_GET['codClient']);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+    <meta http-equiv='pragma' content='no-cache'>
     <title>Remitos pendientes de cobro</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
@@ -23,7 +26,7 @@ $detalleDeRemito = traerDetalle($_GET['codClient']);
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
-    
+
     </link>
 
 </head>
@@ -32,9 +35,12 @@ $detalleDeRemito = traerDetalle($_GET['codClient']);
 
     <div class="alert">
         <div class="page-wrapper bg-secondary p-b-100 pt-2 font-robo">
-            <div class="wrapper wrapper--w680"><div style="color:white; text-align:center"><h6>Remitos pendientes de cobro</h6></div>
+            <div class="wrapper wrapper--w680">
+                <div style="color:white; text-align:center">
+                    <h6>Remitos pendientes de cobro</h6>
+                </div>
                 <div class="card card-1">
-                    
+
                     <div class="row" style="margin-left:1rem">
                         <h3><i class="bi bi-cash" style="margin-right:20px;font-size:50px"></i>Remitos pendientes de cobro</h3>
                     </div>
@@ -43,15 +49,17 @@ $detalleDeRemito = traerDetalle($_GET['codClient']);
                         <div id="divImporteAabonar" style="margin-left: 1rem"><label>Importe a abonar:</label><input class="form-control" type="text" id="importeAbonar" readonly></div>
                         <div id="divImporteConDescuento" style="margin-left: 1rem"> <label>Importe con Descuento:</label><input class="form-control" type="text" id="importeConDescuento" readonly></div>
                         <div style="margin-left: 1rem"><label>% Descuento:</label><input class="form-control" type="text" id="descuento" onchange="calcularDescuento()" placeholder="Colocar números enteros"></div>
-                        <div style="margin-top: 2rem; margin-left: 0.5rem"><button class="btn btn-primary"  value="" id="btnConfirmar">Confirmar <i class="bi bi-check-square"></i></button></div>
+                        <div style="margin-top: 2rem; margin-left: 0.5rem"><button class="btn btn-primary" value="" id="btnConfirmar">Confirmar <i class="bi bi-check-square"></i></button></div>
                         <div style="margin-top: 2rem; margin-left: 1rem"><button class="btn btn-success btn_exportar" id="btnExport"><i class="fa fa-file-excel-o"></i>Exportar<i class="bi bi-file-earmark-excel"></i></button></div>
                     </div>
-                    
+
                     <div class="row" style="margin-left: 1rem;margin-top:1rem">
-                        <div hidden id="codClient"><?=$_GET['codClient'] ?></div>
-                        <div style="width: 500px"><h5 ><strong>Cliente : <p style="color: red;"><?= $detalleDeRemito[0]['RAZON_SOCI'] ?></p></strong></h5></div>
+                        <div hidden id="codClient"><?= $_GET['codClient'] ?></div>
+                        <div style="width: 500px">
+                            <h5><strong>Cliente : <p style="color: red;"><?= $detalleDeRemito[0]['RAZON_SOCI'] ?></p></strong></h5>
+                        </div>
                     </div>
-                    
+
 
                     <table class="table table-striped table-bordered" id="myTable" style="width: 99%;" cellspacing="0" data-page-length="100">
                         <thead class="thead-dark">
@@ -65,81 +73,80 @@ $detalleDeRemito = traerDetalle($_GET['codClient']);
                         </thead>
                         <tbody>
                             <?php
-                                foreach ($detalleDeRemito as $value) {
-                                    if ($value['CHEQUEADO'] == 1){
-                                        continue;
-                                    }
-                                    echo "<tr>";
-                                    echo "<td style='text-align:center' >".$value['FECHA_MOV']->format('Y-m-d')."</td>";
-                                    echo "<td style='text-align:center' >".$value['RAZON_SOCI']."</td>";
-                                    echo "<td style='text-align:center' >".$value['N_COMP']."</td>";
-                                    echo "<td style='text-align:center' id='monto'>".$value['IMPORTE_TO']."</td>";
-                                    echo "<td style='text-align:center;width:20px' ><input type='checkbox' style='width:20px' onchange='checkMonto()' ></td>";
-                                    echo "</tr>";
+                            foreach ($detalleDeRemito as $value) {
+                                if ($value['CHEQUEADO'] == 1) {
+                                    continue;
                                 }
-                        
+                                echo "<tr>";
+                                echo "<td style='text-align:center' >" . $value['FECHA_MOV']->format('Y-m-d') . "</td>";
+                                echo "<td style='text-align:center' >" . $value['RAZON_SOCI'] . "</td>";
+                                echo "<td style='text-align:center' >" . $value['N_COMP'] . "</td>";
+                                echo "<td style='text-align:center' id='monto'>" . $value['IMPORTE_TO'] . "</td>";
+                                echo "<td style='text-align:center;width:20px' ><input type='checkbox' style='width:20px' onchange='checkMonto()' ></td>";
+                                echo "</tr>";
+                            }
+
                             ?>
-                      
+
                         </tbody>
                     </table>
-                
+
+                </div>
             </div>
-        </div>
 
 
 
 
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+            <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+            <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+            <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+            <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
+            <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="js/remitosPendientesDeCobro.js"></script>
-    <script src="js/jquery.table2excel.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+            <script src="js/remitosPendientesDeCobro.js?version=1.0"></script>
+            <script src="js/jquery.table2excel.js"></script>
 
 </body>
 
 </html>
 
 <script>
-    
     const todosLosMontos = document.querySelectorAll('#monto');
-    
-    
+
+
     $(document).ready(function() {
-            parseNumber();
-            $('#myTable').DataTable({
-                responsive: true,
-                buttons: [
-                    'copy', 'csv', 'excel', 'pdf', 'print'
-                ]
-            });
+        parseNumber();
+        $('#myTable').DataTable({
+            responsive: true,
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ]
+        });
 
 
 
-            let total = 0;
+        let total = 0;
 
-            todosLosMontos.forEach(e => {
+        todosLosMontos.forEach(e => {
 
-                total = total + parseInt(e.getAttribute("attr-realValue"))
-                
-            }); 
-            document.querySelector("#totalDeuda").value = "$ "+total.toLocaleString('de-De', {
+            total = total + parseInt(e.getAttribute("attr-realValue"))
+
+        });
+        document.querySelector("#totalDeuda").value = "$ " + total.toLocaleString('de-De', {
             style: 'decimal',
             maximumFractionDigits: 0,
             minimumFractionDigits: 0
         });
-            
+
     });
-        
+
     $("#btnExport").click(function() {
 
-        $('input[type=number]').each(function(){
-            this.setAttribute('value',$(this).val());
+        $('input[type=number]').each(function() {
+            this.setAttribute('value', $(this).val());
         });
 
         $("table").table2excel({
