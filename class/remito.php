45b73c92aca5e04008ba9702c5ae308873588948
sistemas.143 <sistemas@xl.com.ar>
2023-06-05 16:44:42 -0300

    modificaciín query

--- class/remito.php
+++ class/remito.php
@@ -393,9 +393,9 @@ class Remito {
         
 
         $sql = "SELECT B.COD_ARTICU, CAST(SUM(B.CANTIDAD) AS FLOAT) CANTIDAD FROM STA14 A 
-                INNER JOIN STA20 B ON A.NCOMP_IN_S = B.NCOMP_IN_S and A.TCOMP_IN_S = B.TCOMP_IN_S
+                INNER JOIN STA20 B ON A.ID_STA14 = B.ID_STA14
                 INNER JOIN STA11 C ON B.COD_ARTICU = C.COD_ARTICU
-                WHERE A.NCOMP_ORIG = '$rem' AND C.PROMO_MENU != 'M'
+                WHERE A.NCOMP_ORIG = '$rem' AND C.PROMO_MENU != 'P'
                 GROUP BY B.COD_ARTICU
         ";
 
