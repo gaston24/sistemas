061d76a7de43b01061cc736620eaba40fe8d43ea
sistemas.143 <sistemas@xl.com.ar>
2023-06-22 14:01:40 -0300

    Cambios style control remitos

--- control/controlRemitos.php
+++ control/controlRemitos.php
@@ -24,6 +24,7 @@ if (!isset($_SESSION['username'])) {
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<!-- Font Awesome -->
 		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 		<link rel="stylesheet" href="css/style.css">
 		<?php require_once __DIR__.'/../assets/css/header.php';?>
 		<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -34,11 +35,11 @@ if (!isset($_SESSION['username'])) {
 	<body>
 
 
-		<div style="margin-top: 10px">
+		<div>
 		<div id="contBtn">
-			<button onClick="window.location.href= 'index.php'" class="btn btn-danger mt-2">Cancelar</button>
-			<button class="btn btn-secondary mt-2" id="btnBorrador" hidden>Guardar Borrador</button>
-			<button class="btn btn-dark mt-2" id="btnTraerBorrador" hidden>Traer Borrador</button>
+			<button onClick="window.location.href= 'index.php'" class="btn btn-danger mt-2">Cancelar <i class="bi bi-x-circle"></i></button>
+			<button class="btn btn-secondary mt-2" id="btnBorrador" hidden>Guardar <i class="bi bi-card-list"></i></button>
+			<button class="btn btn-dark mt-2" id="btnTraerBorrador" hidden>Traer <i class="bi bi-card-list"></i></button>
 		</div>
 			<h6 align="center">Remito: <a class="text-secondary" id="numRem"><?= $rem ?></a> 
 				<!-- <a>Ultimo escaneado:</a>
@@ -51,7 +52,7 @@ if (!isset($_SESSION['username'])) {
 		<form id="formControlRemito">
 			<div class="col- row" style="display: flex; justify-content: center;">
 				<input class="form-control form-control-sm ml-2" type="text" name="codigo" id="codigoName" placeholder="Ingrese codigo..." style="width:200px;" autofocus></input>
-				<input type="submit" value="Ingresar" class="btn btn-primary ml-2" style="margin-top: -5px;">
+				<button type="submit" class="btn btn-primary ml-2" style="margin-top: -5px;">Ingresar <i class="bi bi-plus-circle"></i></button>
 			</div>
 		</form>
 
@@ -74,9 +75,9 @@ if (!isset($_SESSION['username'])) {
 		</table>
 	</div>
 		<div class="col- text-center bg-white mt-1">
-			<a style="text-align: left; margin-right:0.5em; font-size: 0.8em"> <strong>Ult.:</strong> <a id="lastCodigoControlado" style="font-size: 0.8em"></a> <button id="buttonHistorial" type="button" class="btn btn-info btn-sm mr-3" >Ver</button></a>
-			<a style="margin-right:0.5em; font-size: 0.8em"> <strong>Art.:</strong></a> <a id="totalArt"></a>
-			<button class="btn btn-success btn-sm" id="btnProcesar">Procesar</button>
+			<a style="text-align: left; margin-right:0.5em; font-size: 0.8em"> <strong>Ult.:</strong> <a id="lastCodigoControlado" style="font-size: 0.8em"></a> <button id="buttonHistorial" type="button" class="btn btn-info btn-sm mr-3" >Ver <i class="bi bi-search"></i></button></a>
+			<a style="margin-right:0.3em; font-size: 0.8em"> <strong>Art.:</strong></a> <a id="totalArt"></a>
+			<button class="btn btn-success btn-sm" id="btnProcesar">Procesar <i class="bi bi-check2-circle"></i></button>
 		</div>
 
 	
