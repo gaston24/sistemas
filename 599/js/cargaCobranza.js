148fe70f6fcfbe2d94800793e722fa85bee3883d
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-08-24 09:18:35 -0300

    24/8

--- 599/js/cargaCobranza.js
+++ 599/js/cargaCobranza.js
@@ -90,7 +90,7 @@ const confirmarCobro = (codClient) => {
 
                         }else {
                             Swal.fire('Error!', 'El cobro ya existe en la base de datos, porfavor regrese a la pantalla de seleccion y recargue', 'error').then((result) => {
-                                window.location.href = "seleccionCobranza.php";
+                                window.location.href = "composicionDeRemitos.php";
                             });
                         }
 
