f7662b026158f0a8e2eab17533205efdd9d8280e
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-09-06 08:47:38 -0300

    1

--- control/estadoControlRemitos.php
+++ control/estadoControlRemitos.php
@@ -214,7 +214,6 @@ $data = $control->traerEstadoControlRemitos($desde, $hasta, $sucursal, $estado);
 <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-<link rel="stylesheet" type="text/css" href="assets/select2/select2.min.css">
 <script src="js/estadoControlRemitos.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
@@ -235,10 +234,10 @@ $data = $control->traerEstadoControlRemitos($desde, $hasta, $sucursal, $estado);
     });
     $('.selectSucursal').select2();
 
-    $(document).ready(function() {
+    // $(document).ready(function() {
 
-        document.querySelector('.select2-selection.select2-selection--single').style.height = "40px";
+    //     // document.querySelector('.select2-selection.select2-selection--single').style.height = "40px";
 
-    })
+    // })
 
 </script>
