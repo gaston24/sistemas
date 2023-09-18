86158b6bf71bc94213beb80aedd5e6eb5cbb7078
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-09-18 14:59:20 -0300

    18/9

--- egresosDeCaja/upload_image.php
+++ egresosDeCaja/upload_image.php
@@ -15,63 +15,64 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_FILES['archivos']['name'][
     $totalFiles = count($_FILES['archivos']['name']);
 
     $uploadedFiles = 0;
-    $maxFileSize = 2 * 1024 * 1024; // 2 MB en bytes
-  
+    $maxFileSize = 4 * 1024 * 1024; // 4 MB en bytes
+
     for ($i = 0; $i < $totalFiles; $i++) {
 
         // Verificar el tamaño del archivo
         if ($_FILES['archivos']['size'][$i] <= $maxFileSize) {
 
-        // Obtener el timestamp actual en milisegundos
-        $timestamp = strval(round(microtime(true) * 1000));  
+            // Obtener el timestamp actual en milisegundos
+            $timestamp = strval(round(microtime(true) * 1000));  
 
-        $newName =  $_FILES['archivos']['name'][$i]."_".$i."_".$timestamp.'.jpg';
+            $newName =  $_FILES['archivos']['name'][$i]."_".$i."_".$timestamp.'.jpg';
 
-        $targetFile = $targetDir . basename($newName);
-        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+            $targetFile = $targetDir . basename($newName);
+            $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-        // Permitir solo ciertos tipos de archivos
-        $allowedTypes = array('pdf', 'jpg', 'png');
-        if (in_array($fileType, $allowedTypes)) {
-            if (move_uploaded_file($_FILES['archivos']['tmp_name'][$i], $targetFile)) {
+            // Permitir solo ciertos tipos de archivos
+            $allowedTypes = array('pdf', 'jpg', 'png');
+            if (in_array($fileType, $allowedTypes)) {
+                if (move_uploaded_file($_FILES['archivos']['tmp_name'][$i], $targetFile)) {
 
-                // Obtener las dimensiones originales de la imagen
-                list($originalWidth, $originalHeight) = getimagesize($targetFile);
-                
+                    // Obtener las dimensiones originales de la imagen
+                    list($originalWidth, $originalHeight) = getimagesize($targetFile);
+                    
 
-                if ($originalWidth > 800 || $originalHeight > 600) {
-                    $newWidth = 960;
-                    $newHeight = 1360;
+                    if ($originalWidth > 800 || $originalHeight > 600) {
+                        $newWidth = 960;
+                        $newHeight = 1360;
 
-                    $resizedImage = imagecreatetruecolor($newWidth, $newHeight);
-                    $sourceImage = imagecreatefromjpeg($targetFile); // Cambia a la función adecuada según el tipo de imagen
+                        $resizedImage = imagecreatetruecolor($newWidth, $newHeight);
+                        $sourceImage = imagecreatefromjpeg($targetFile); // Cambia a la función adecuada según el tipo de imagen
 
-                    imagecopyresized(
-                        $resizedImage,
-                        $sourceImage,
-                        0,
-                        0,
-                        0,
-                        0,
-                        $newWidth,
-                        $newHeight,
-                        $originalWidth,
-                        $originalHeight
-                    );
+                        imagecopyresized(
+                            $resizedImage,
+                            $sourceImage,
+                            0,
+                            0,
+                            0,
+                            0,
+                            $newWidth,
+                            $newHeight,
+                            $originalWidth,
+                            $originalHeight
+                        );
 
-                    imagejpeg($resizedImage, $targetFile);
+                        imagejpeg($resizedImage, $targetFile);
 
-                    imagedestroy($sourceImage);
-                    imagedestroy($resizedImage);
+                        imagedestroy($sourceImage);
+                        imagedestroy($resizedImage);
+                    }
+
+
+
+                    $uploadedFiles++;
                 }
-
-
-
-                $uploadedFiles++;
             }
-        }
+
         } else {
-            echo "Error: El archivo excede el tamaño máximo permitido.";
+            echo "Error: El archivo excede el tamaño máximo permitido ( 4 MB ).";
         }
     }
 
