18c3bf02c1f050e5dcd1161f4752e94ce3a3efbc
sistemas.143 <sistemas@xl.com.ar>
2024-02-28 12:38:09 -0300

    2

--- maestroDestinos/Class/Articulo.php
+++ maestroDestinos/Class/Articulo.php
@@ -14,9 +14,15 @@ class Articulo
         require_once $_SERVER['DOCUMENT_ROOT'].'/sistemas/class/conexion.php';
         
         $this->cid = new Conexion();
-       
-        $db = (isset($_SESSION['usuarioUy']) && $_SESSION['usuarioUy'] == 1) ? 'uy' : 'central';
+
+        if (session_status() === PHP_SESSION_NONE) {
+           
+            session_start();
+
+        }
         
+        $db = (isset($_SESSION['usuarioUy']) && $_SESSION['usuarioUy'] == 1) ? '' : 'central';
+
         $this->cid_central = $this->cid->conectar($db);
 
     } 
