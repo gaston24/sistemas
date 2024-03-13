3e808db50d59793476740bd5e153e5e0747fe68d
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2024-03-13 14:34:43 -0300

    1

--- control/controlHistoricosDetalle.php
+++ control/controlHistoricosDetalle.php
@@ -37,7 +37,9 @@ $remitosHistoricosDetalle = $remitos->traerHistoricosDetalle($numRem);
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 
+<script src="js/jquery.table2excel.js"></script>
 
+<script src="https://cdn.jsdelivr.net/npm/table2excel@1.0.4/dist/table2excel.min.js"></script>
 
 </head>
 <body>	
@@ -56,7 +58,9 @@ if(isset($_GET['numRem'])){
 <div class="row">
 	<div class="col-2"><button class="btn btn-primary btn-sm mt-1 ml-5 mb-1" onclick="location.href= document.referrer ">Volver</button></div>
 	<div class="col-8" id="datosRemito"></div>
-	<div class="col-2"></div>
+	<div class="col-2">
+	<div style="margin-left: 1rem;"><button class="btn btn-success btn_exportar" id="btnExport" onclick="exportar('<?= $numRem ?>')"><i class="fa fa-file-excel-o"></i> Exportar<i class="bi bi-file-earmark-excel"></i></button></div>
+	</div>
 </div>
 
 <table class="table table-striped"  id="tabla">
@@ -127,7 +131,22 @@ if(isset($_GET['numRem'])){
 
 	$(document).ready(function(){
 		$('#datosRemito').html('<h4><?=$codSuc?> (<?=$numSuc?> ) - <?=$numRem?> - <?=$nombreVen?></h4>');
+
+		
 	});
+
+	const exportar = (numRem) => {
+
+		$("table").table2excel({
+
+			// exclude CSS class
+			exclude: ".noExl",
+			name: "Worksheet Name",
+			filename: `Detalle - ${numRem}`, //do not include extension
+			fileext: ".xls", // file extension
+		});
+
+	};
 </script>
 </body>
 </html>
