f66848591f87e461a0601201f24d781ecbb995c8
gaston24 <gaston.marcilio@gmail.com>
2024-07-26 12:25:36 -0300

    1

--- controlFallas/enviarSolicitud.php
+++ controlFallas/enviarSolicitud.php
@@ -1,7 +1,7 @@
 <?php 
 
     session_start();
-    if(!isset($_SESSION['username']) || ($_SESSION['usuarioUy'] == 1)){
+    if(!isset($_SESSION['username']) ){
         header("Location:login.php");
     }
 
