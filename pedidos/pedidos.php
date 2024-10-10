a8cc52ccdc86768576b391db887cbdb3ce342360
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2024-10-10 11:16:46 -0300

    410

--- pedidos/pedidos.php
+++ pedidos/pedidos.php
@@ -239,7 +239,7 @@ if (!isset($_SESSION['username'])) {
                             <td id="cantVendida" class="ventas-30-dias">0</td>
                             <td id="cant"><?= (int)($v['DISTRI']) ?></td>
                             <td>
-                                <input type="number" name="cantPed[]" class="form-control form-control-sm pedido-input" value="0" min="0" id="articulo" onchange="total();verifica();precioTotal()" <?= ((int)$v['DISTRI'] > 0 && $_SESSION['tipo'] != 'FRANQUICIA') ? 'disabled' : '' ?>>
+                                <input type="number" name="cantPed[]" class="form-control form-control-sm pedido-input" value="0" min="0" id="articulo" onchange="total();verifica();precioTotal()" <?= ((int)$v['DISTRI'] > 0 && $_SESSION['tipo'] != 'FRANQUICIA' && $v['RUBRO'] != 'PACKAGING') ? 'disabled' : '' ?>>
                             </td>
                             <td id="precio">
                             <?php
